7712a5d938a506826e500ae0502c7db5c7ac6828
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-01-06 09:36:39 +0530

    Update TitleBar.js

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -4,7 +4,15 @@ import React from "react";
 export default function TitleBar() {
   return (
     <Heading>
-      <Img src="./Logo.jpg" htmlWidth="100%" htmlHeight="auto" />
+      <Img
+        src="./Logo.jpg"
+        htmlWidth="100%"
+        htmlHeight="auto"
+        alt="logo"
+        px={3}
+        mt={2}
+        mb={3}
+      />
     </Heading>
   );
 }
