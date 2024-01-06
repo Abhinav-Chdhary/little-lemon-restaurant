02f4abd24b3d73357b0c14a3bd4d5ca80f681a8c
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-01-06 09:32:55 +0530

    Bg-color and homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,10 +1,18 @@
-import { VStack, Text } from "@chakra-ui/react";
+import { VStack, Text, Img } from "@chakra-ui/react";
 import React from "react";
 
 export default function HomePage() {
   return (
     <VStack>
-      <Text>
+      <Img src="./Grill2.jpg" m={2} rounded="lg" pos={"fixed"} />
+      <Text
+        color={"#edeeef"}
+        bgColor={"#ee9972"}
+        rounded={"lg"}
+        padding={2}
+        zIndex={100}
+        ml={2}
+      >
         Little Lemon is a charming neighbourhood bistro that serves simple food
         and classic cocktails in a lively but casual environment. The restaurant
         features a locally-sourced menu with daily specials.
