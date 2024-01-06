463053e4339506c7b86953a06ed871434e8da8ff
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-01-06 09:12:42 +0530

    Added chakra.ui, routing, images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from "react";
-
+import { ChakraProvider } from "@chakra-ui/react";
+import HomePage from "./Pages/HomePage";
+import BookPage from "./Pages/BookPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 export default function App() {
-  return <div>App</div>;
+  return (
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/" element={<BookPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
+  );
 }
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,14 @@
+import { VStack, Text } from "@chakra-ui/react";
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <VStack>
+      <Text>
+        Little Lemon is a charming neighbourhood bistro that serves simple food
+        and classic cocktails in a lively but casual environment. The restaurant
+        features a locally-sourced menu with daily specials.
+      </Text>
+    </VStack>
+  );
+}
